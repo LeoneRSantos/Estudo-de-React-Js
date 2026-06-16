5f7b9e2a501c4f0c8862be61afa35cbc4082cc4c
LeoneRSantos <leonnerodriguez1@gmail.com>
2026-06-16 11:37:39 -0300

    Adição da propriedade contato em App

--- projeto-base/src/App.tsx
+++ projeto-base/src/App.tsx
@@ -77,7 +77,7 @@ function App() {
           <svg className="icon" role="presentation" aria-hidden="true">
             <use href="/icons.svg#social-icon"></use>
           </svg>
-          <h2>Connect with us</h2>
+          <h2>{novasVariaveisEmUso.contato}</h2>
           <p>Join the Vite community</p>
           <ul>
             <li>
