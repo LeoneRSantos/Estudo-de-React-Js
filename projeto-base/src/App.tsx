f308c3c0676b0d76c8970d21ea6bb6872bec3a23
LeoneRSantos <leonnerodriguez1@gmail.com>
2026-06-16 11:36:53 -0300

    Adição da propriedade Count em App

--- projeto-base/src/App.tsx
+++ projeto-base/src/App.tsx
@@ -45,7 +45,7 @@ function App() {
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          Count is {count}
+          {variaveisEmUso.Count}  {count}
         </button>
       </section>
 
